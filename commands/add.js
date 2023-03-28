ee535f1cf9e5d9877cc13f819c9393c3ef6e08b4
Jordan Rogers <jrdnrgrs@gmail.com>
2023-03-28 18:23:03 -0500

    add command for adding personalities

---
+++ commands/add.js
@@ -0,0 +1,46 @@
+// Requre the necessary discord.js classes
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+const { DISABLED_MSG,  } = require('../constants');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('add-personality')
+        .setDescription('Add a new personality to the bot.')
+        .addStringOption(option =>
+            option.setName('name')
+                .setDescription('The name of the new personality.')
+                .setRequired(true))
+        .addStringOption(option =>
+            option.setName('prompt')
+                .setDescription('The prompt for the new personality.')
+                .setRequired(true)),
+    async execute(interaction, state) {
+        // Commands to execute
+        // Check admin/pause state
+        if (!interaction.member.permissions.has(PermissionFlagsBits.ManageMessages) && state.isPaused === true) {
+            await interaction.reply(DISABLED_MSG);
+            return;
+        }
+        const name = interaction.options.getString('name');
+        const prompt = interaction.options.getString('prompt');
+
+        // Check if personality already exists
+        const existingPersonality = state.personalities.find(p => p.name.toUpperCase() === name.toUpperCase());
+        if (existingPersonality) {
+            await interaction.reply('A personality with this name already exists. Please choose a different name.');
+            return;
+        }
+
+        // Add the new personality
+        state.personalities.push({
+            name: name,
+            prompt: prompt,
+            request: [{
+                "role": "system",
+                "content": `${prompt}`
+            }]
+        });
+
+        await interaction.reply(`New personality "${name}" added.`);
+    },
+};
